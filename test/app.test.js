ea9ece0c664616a78971ee505c6b069578111ba6
Gurjot <kaurgurjot707@gmail.com>
2025-11-14 00:46:27 -0800

    tests: expect homepage HTML (includes 'Welcome to a simple Node.js App!')

--- test/app.test.js
+++ test/app.test.js
@@ -1,18 +1,22 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../app');
+const expect = chai.expect;
+const server = require('../app');
 
 chai.use(chaiHttp);
-const expect = chai.expect;
 
-describe('GET /', () => {
-  it('should return Hello, World!', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.text).to.equal('Hello, World! Your app is running!');
-        done();
-      });
+describe('Simple Web App Tests', () => {
+  it('GET / should return the HTML homepage', (done) => {
+    chai.request(server).get('/').end((err, res) => {
+      expect(res).to.have.status(200);
+      // Look for something that is definitely on your page:
+      expect(res.text).to.include('Welcome to a simple Node.js App!');
+      done();
+    });
+  });
+
+  // Cleanly close the server if it’s running
+  after(() => {
+    if (server && server.close) try { server.close(); } catch (_) {}
   });
 });
